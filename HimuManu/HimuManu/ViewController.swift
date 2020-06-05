a7201daa77537af413d01432c4909501a2ae994a
Rohit <rohitttiwari415.rt@gmail.com>
2020-06-05 20:05:45 +0530

    myproject

---
+++ HimuManu/HimuManu/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  HimuManu
+//
+//  Created by apple on 05/06/20.
+//  Copyright © 2020 com.rohit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+  var h = "heklo"
+    
+}
+
